11fa7cd129805e4bc507393080bbb4f71cd30be7
Leonardum <trennel2@hotmail.com>
2019-09-07 16:51:31 +0300

    Add files via upload

--- temp/99ade512edfabc7d8d7c7704a7d91e04f3eb9c3fce8f4101d6ced76411ac8a75.php
+++ temp/99ade512edfabc7d8d7c7704a7d91e04f3eb9c3fce8f4101d6ced76411ac8a75.php
@@ -1,1 +1,18 @@
+<?php
 
+	session_start();
+	
+	$_SESSION['userId'] = 5;
+	
+	require('../model/database.php');
+	require_once('../model/user_object.php');
+	
+	$user = user::get_userById(5);
+	
+	$user->setAccountActivated(1);
+	
+	$user->save();
+	
+	header('Location: ../index.php?action=welcome');
+	
+	?>
